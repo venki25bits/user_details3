91c2d517f9c9d984d0fd268f789d39e671caefad
VaishnaviBejjanki <vaishnavibejjanki@gmail.com>
2022-05-07 16:52:24 +0530

    get-mongo

--- pkg/db/mongo/mongo.go
+++ pkg/db/mongo/mongo.go
@@ -8,3 +8,12 @@ type Mongo struct {
 	mgo.Mongo
 	Collection string
 }
+
+func (ss *Mongo) FindUser(userId string, ctx context.Context) (model.User, error){
+	var user model.User
+	collection := ss.Database(ctx, bson.M{"userId":userId}).Decode(&user)
+	if err != nil{
+		return user, nil
+	}
+	return user, nil
+}
