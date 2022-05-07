622be58e8d29575cb2025d2125141b2f3e7000c9
VaishnaviBejjanki <vaishnavibejjanki@gmail.com>
2022-05-07 14:15:43 +0530

    added data model

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,1 +1,8 @@
 package model
+
+id string `bson:"id" json:"id"`
+firstName string `bson:"firstName" json:"firstName"`
+lastName string `bson:"lastName" json:"lastName"`
+userName string `bson:"userName" json:"userName"`
+emailId string `bson:"emailId" json:"emailId"`
+password string `bson:"password" json:"password"` 
